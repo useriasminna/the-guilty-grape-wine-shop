df88faf58efd0772b7cac877aeb7648713a38774
unknown <pal.iasmina@yahoo.com>
2022-10-04 16:09:00 +0100

    Add Deploy settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn the_guilty_grape.wsgi:application 
--- the_guilty_grape/settings.py
+++ the_guilty_grape/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -108,12 +109,18 @@ WSGI_APPLICATION = 'the_guilty_grape.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+    'default' : dj_database_url.parse(os.environ.get('DATABASE_URL')) 
 }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
