7dc8155495b269eac8ae212681a72a4909a2569a
unknown <pal.iasmina@yahoo.com>
2022-10-04 16:16:39 +0100

    Removed secret key and set debug

--- the_guilty_grape/settings.py
+++ the_guilty_grape/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$e@w&#3q5-@+_ka+f1xukr)d5!$to4qq^viuc4f+snxeb8)_j&'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['the-guilty-grape-wine-shop.herokuapp.com', 'localhost']
 
 
 # Application definition
