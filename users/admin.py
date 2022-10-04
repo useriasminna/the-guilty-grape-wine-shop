24b4f971f0e910ef18e75c9ffd598231d87d0213
unknown <pal.iasmina@yahoo.com>
2022-10-04 15:28:57 +0100

    Create custom user admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,38 @@
+"""
+Users App - Admin
+----------------
+Admin Configuration for Users App.
+"""
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+User = get_user_model()
+
+# Remove Group Model from admin. We're not using it.
+admin.site.unregister(Group)
+
+
+class UserAdmin(BaseUserAdmin):
+    """Custom Use Admin"""
+    list_display = ['email', 'admin', 'first_name', 'last_name']
+    list_filter = ['admin']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ()}),
+        ('Permissions', {'fields': ('admin',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }
+        ),
+    )
+    search_fields = ['email']
+    ordering = ['email']
+    filter_horizontal = ()
+
+admin.site.register(User, UserAdmin)
