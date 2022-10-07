56e36d2829c60c232b7c8fbcf46afa563bc44573
unknown <pal.iasmina@yahoo.com>
2022-10-07 15:50:25 +0100

    Add file docstring

--- custom_storages.py
+++ custom_storages.py
@@ -1,10 +1,18 @@
+
+"""
+Custom Storages Configuration
+"""
+
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
+    """Set static files location"""
     location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
+    """Set media files location"""
     location = settings.MEDIAFILES_LOCATION
+    
