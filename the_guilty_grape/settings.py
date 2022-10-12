6de032d4c54447df04c8baa8bd6d7546bdc1f069
unknown <pal.iasmina@yahoo.com>
2022-10-12 19:26:12 +0100

    Retry commiting setting due to heroku error

--- the_guilty_grape/settings.py
+++ the_guilty_grape/settings.py
@@ -180,7 +180,7 @@ if 'USE_AWS' in os.environ:
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
-    
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'the-guilty-grape-wine-shop'
     AWS_S3_REGION_NAME = 'eu-west-1'
