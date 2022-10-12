70793152df48818c11db30e5a7a3050a64742251
unknown <pal.iasmina@yahoo.com>
2022-10-12 16:54:51 +0100

    Add script for positioning the main container bellow header

--- static/js/script.js
+++ static/js/script.js
@@ -18,3 +18,8 @@ navbarTogglerButton.addEventListener('click', () => {
         }, 350);
     }
 });
+
+// SCRIPT FOR SETTING PADDING TOP OF CONTENT CONTAINER TO BE EQUAL WITH HEADER HEIGHT
+let headerHeight = document.getElementsByTagName('header')[0].offsetHeight;
+let contentContainer = document.getElementsByClassName('content-container')[0];
+contentContainer.style.paddingTop= (headerHeight - 2) + 'px';
