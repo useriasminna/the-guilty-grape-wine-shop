c858aa9a6b9b860c79f4ba93e314e4dc081250e5
unknown <pal.iasmina@yahoo.com>
2022-10-14 14:54:46 +0100

    Update email settings

--- the_guilty_grape/settings.py
+++ the_guilty_grape/settings.py
@@ -209,8 +209,8 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'theguiltygrape@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
+    EMAIL_USE_SSS = True
+    EMAIL_PORT = 465
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
