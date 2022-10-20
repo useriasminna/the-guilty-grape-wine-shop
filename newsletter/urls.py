9303448caccb4a954562bc250d939ec2c09e5d00
unknown <pal.iasmina@yahoo.com>
2022-10-20 13:37:05 +0100

    Create path for newsletter subscription form

---
+++ newsletter/urls.py
@@ -0,0 +1,11 @@
+"""
+Newsletter App - Urls
+----------------
+Urls for Newsletter App.
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.SubscribeToNewsletter.as_view(), name='subscribe_to_newsletter'),
+]
