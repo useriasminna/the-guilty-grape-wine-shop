02fbd8d7ef8b8281f2bec27c41673265768db08a
unknown <pal.iasmina@yahoo.com>
2022-10-20 13:35:54 +0100

    Register Subscription model to admin pane

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,11 @@
+"""
+Newsletter App - Admin
+----------------
+Admin Configuration for Newsletter App.
+"""
 from django.contrib import admin
 
+from newsletter.models import Subscription
+
 # Register your models here.
+admin.site.register(Subscription)
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,47 @@
+"""
+Newsletter App - Views
+----------------
+Views for Newsletter App.
+"""
+from django.views.generic import CreateView
+
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
+from django.contrib import messages
+
+from newsletter.forms import AddSubscriber
+from newsletter.models import Subscription
 
 # Create your views here.
+class SubscribeToNewsletter(CreateView):
+    """A view that provides a form for users to subscribe for newsletter"""
+    template_name = "base.html"
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['path'] = self.kwargs.get('path')
+        return kwargs
+
+    def post(self, request, *args, **kwargs):
+        """Override post method"""
+        if request.method == 'POST':
+
+            subscribe_form = AddSubscriber(request.POST, request.FILES)
+
+            if subscribe_form.is_valid():
+                email = subscribe_form.cleaned_data['email']
+                subscription = Subscription(email=email)
+                subscription.save()
+                messages.success(
+                    request, 'Thank you for you subscription!', extra_tags="form_success")
+                return HttpResponseRedirect(
+                    request.POST.get('newsletter_submit_btn','') + '#newsletter')
+            else:
+                messages.error(request, subscribe_form.errors['email'], extra_tags="form_errors")
+                return HttpResponseRedirect(
+                    request.POST.get('newsletter_submit_btn','') + '#newsletter')
+            #  return HttpResponse(
+                # subscribe_form.errors.as_json(), status = 400, content_type='application/json')
+
+        subscribe_form = AddSubscriber()
+        return render(request, 'base.html', {'add_subscriber_forms': subscribe_form, })
