cebfc3fe04cfdfc2d7799bb8af39a79741fd0973
unknown <pal.iasmina@yahoo.com>
2022-10-20 13:34:05 +0100

    Create Subscription model for newsletter

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,3 +1,16 @@
+"""
+Newsletter App - Models
+----------------
+Models for Newsletter App.
+"""
+
 from django.db import models
 
 # Create your models here.
+class Subscription(models.Model):
+    """Model for Subscription object"""
+    email = models.EmailField(unique=True,)
+
+    def __str__(self):
+        return str(self.email)
+    
