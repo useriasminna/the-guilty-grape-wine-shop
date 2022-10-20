0d171dc70fef468f4e8b0f2cc757871313a56775
unknown <pal.iasmina@yahoo.com>
2022-10-20 13:37:36 +0100

    Add subscription form to context processors

---
+++ the_guilty_grape/context_processors.py
@@ -0,0 +1,10 @@
+"""
+the_guilty_grape project - Context processors
+"""
+from newsletter.forms import AddSubscriber
+
+def add_subscription_form_to_context(request):
+    """Method to return subscription form as context"""
+    return{
+        'add_subscriber_form' : AddSubscriber
+    }
