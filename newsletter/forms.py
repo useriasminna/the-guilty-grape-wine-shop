78620edfe794c44b8453d5e618b5611107d13fad
unknown <pal.iasmina@yahoo.com>
2022-10-20 13:34:44 +0100

    Create form for Subscription model

---
+++ newsletter/forms.py
@@ -0,0 +1,16 @@
+"""
+Newsletter App - Forms
+----------------
+Forms for Newsletter App.
+"""
+from django import forms
+from .models import Subscription
+
+class AddSubscriber(forms.ModelForm):
+    """Form for adding email to newsletter subscribers"""
+    email = forms.EmailField(widget=forms.TextInput(attrs={'id':'newsletter_email',}),label="Enter your email:")
+
+    class Meta:
+        """Meta class"""
+        model = Subscription
+        fields = ['email',]
