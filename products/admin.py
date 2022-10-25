c541925168ee0cf6c71f0ae2d6e6d24109eb0fcf
unknown <pal.iasmina@yahoo.com>
2022-10-25 11:53:32 +0100

    Customize admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,29 @@
+"""
+Products App - Admin Panel Configuration
+"""
 from django.contrib import admin
-
+from .models import Product,Category
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    """Class for displaying products in admin panel"""
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'country',
+        'year',
+        'price',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    """Class for displaying categories in admin panel"""
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
