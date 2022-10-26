47a3542c8016d69897d90cd16d42e225cf3f4807
unknown <pal.iasmina@yahoo.com>
2022-10-26 14:27:05 +0100

    Include products app urls

--- the_guilty_grape/urls.py
+++ the_guilty_grape/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from pathlib import Path
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -23,4 +24,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('subscribe/', include('newsletter.urls')),
+    path('products/', include('products.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
