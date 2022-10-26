ea5024d465b1afea70b6265e65b42a9fad51c4a7
unknown <pal.iasmina@yahoo.com>
2022-10-26 14:24:36 +0100

    Add script for product count group

--- static/js/script.js
+++ static/js/script.js
@@ -23,3 +23,29 @@ navbarTogglerButton.addEventListener('click', () => {
 let headerHeight = document.getElementsByTagName('header')[0].offsetHeight;
 let contentContainer = document.getElementsByClassName('content-container')[0];
 contentContainer.style.paddingTop= (headerHeight - 2) + 'px';
+
+
+// SCRIPT FOR PRODUCT COUNT BUTTONS FOR ADDITION AND SUBSTRACTION TO UPDATE INPUT VALUE ON CLICK
+let productCountContainers = document.getElementsByClassName('product-count');
+
+for(let container of productCountContainers){
+    let buttons = container.getElementsByTagName('button');
+    for (let btn of buttons){
+        btn.addEventListener('click', (e) => {
+            let input = e.target.parentElement.getElementsByTagName('input')[0];
+            let min = input.min;
+            let max = input.max;
+            if (e.target.classList.contains('substraction')){
+                if(parseInt(input.value) > min){
+                    input.setAttribute('value', parseInt(input.value) - 1);
+                }
+            }  
+            else if (e.target.classList.contains('addition')){
+                if(parseInt(input.value) < parseInt(max)){
+                    input.setAttribute('value', parseInt(input.value) + 1);        
+                }
+            }    
+        });
+    }
+    
+}
