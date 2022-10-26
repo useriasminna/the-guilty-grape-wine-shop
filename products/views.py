e8038a5195ac07545b353200e37e3b07628ac3d9
unknown <pal.iasmina@yahoo.com>
2022-10-26 14:21:50 +0100

    Add views for all products display

--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+"""
+Products App - Views
+----------------
+Views for Products App.
+"""
+from django.views.generic import ListView
+from .models import Product
 
-# Create your views here.
+
+class Products(ListView):
+    """ A view to show all products, including sorting and search queries """
+    template_name = "products/products.html"
+    model = Product
+    paginate_by = 12
+    context_object_name = "products_list"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['products'] = Product.objects.all()
+        return context
