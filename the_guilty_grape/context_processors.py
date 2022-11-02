29ab8439df2a44b85f70efa4187607a03101c72f
unknown <pal.iasmina@yahoo.com>
2022-11-02 12:10:33 +0000

    Fix linter issues

--- the_guilty_grape/context_processors.py
+++ the_guilty_grape/context_processors.py
@@ -3,8 +3,9 @@ the_guilty_grape project - Context processors
 """
 from newsletter.forms import AddSubscriber
 
+
 def add_subscription_form_to_context(request):
     """Method to return subscription form as context"""
-    return{
-        'add_subscriber_form' : AddSubscriber
+    return {
+        'add_subscriber_form': AddSubscriber
     }
