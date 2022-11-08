7ecb1d0b5d23c9fe489baf129d0d4e7968a25305
unknown <pal.iasmina@yahoo.com>
2022-11-08 15:11:52 +0000

    Fix linter errors

--- products/admin.py
+++ products/admin.py
@@ -2,8 +2,9 @@
 Products App - Admin Panel Configuration
 """
 from django.contrib import admin
-from .models import Product,Category
+from .models import Product, Category
 # Register your models here.
+
 
 class ProductAdmin(admin.ModelAdmin):
     """Class for displaying products in admin panel"""
@@ -18,12 +19,14 @@ class ProductAdmin(admin.ModelAdmin):
 
     ordering = ('sku',)
 
+
 class CategoryAdmin(admin.ModelAdmin):
     """Class for displaying categories in admin panel"""
     list_display = (
         'friendly_name',
         'name',
     )
+
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
