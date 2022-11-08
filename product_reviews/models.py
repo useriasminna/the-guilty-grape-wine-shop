7eb77a8e1f281c098f22c50d06e626ae2117cabf
unknown <pal.iasmina@yahoo.com>
2022-11-08 15:02:32 +0000

    Create Review model

--- product_reviews/models.py
+++ product_reviews/models.py
@@ -1,3 +1,32 @@
+"""
+Product Reviews App - Models
+----------------
+Models for Product Reviews App.
+"""
+import datetime
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from products.models import Product
+
+
+class Review(models.Model):
+    """Model for Product Review Post"""
+    rate = models.PositiveSmallIntegerField()
+    review_text = models.TextField()
+    now = datetime.datetime.now()
+    date_created_on = models.DateTimeField(
+        default=now.strftime("%Y-%m-%d %H:%M:%S"))
+    date_updated_on = models.DateTimeField(
+        default=now.strftime("%Y-%m-%d %H:%M:%S"))
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        to_field='email', blank=True)
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, blank=True)
+
+    class Meta:
+        ordering = ["date_updated_on"]
+
+    def __str__(self):
+        return f"Review {self.review_text} by {self.author}"
