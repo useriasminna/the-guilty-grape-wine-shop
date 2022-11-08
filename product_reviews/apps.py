8b5e5a50d6a86240ae0de6bf17c018387bf70558
unknown <pal.iasmina@yahoo.com>
2022-11-08 15:01:19 +0000

    Add docstrings

--- product_reviews/apps.py
+++ product_reviews/apps.py
@@ -1,6 +1,13 @@
+"""
+Product Review App - Apps
+----------------
+App Configuration for Product Review App.
+"""
+
 from django.apps import AppConfig
 
 
 class ProductReviewsConfig(AppConfig):
+    """App configuration"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'product_reviews'
