9bd13d0c1efbbe7dcda5546e1fa34ba3fc2c1f72
unknown <pal.iasmina@yahoo.com>
2022-11-09 16:01:46 +0000

    Add unique atributes to sku and code fields

--- products/models.py
+++ products/models.py
@@ -28,14 +28,14 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True,
                                  on_delete=models.SET_NULL)
     is_deluxe = models.BooleanField()
-    sku = models.CharField(max_length=254)
+    sku = models.CharField(max_length=254, unique=True)
     name = models.CharField(max_length=254)
     region = models.TextField(max_length=100)
     country = models.TextField(max_length=100)
     grapes = models.TextField(max_length=254)
     year = models.IntegerField()
     style = models.TextField()
-    code = models.CharField(max_length=6)
+    code = models.CharField(max_length=6, unique=True)
     food_pairing = models.TextField(max_length=60)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
