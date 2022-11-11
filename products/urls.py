43ba47e29bba36213173dc0dfb3ed9428971d2f0
unknown <pal.iasmina@yahoo.com>
2022-11-11 12:13:01 +0000

    Add paths for product add and remove product to wishlist

--- products/urls.py
+++ products/urls.py
@@ -5,7 +5,7 @@ Urls for Products App.
 """
 from django.urls import path
 from . import views
-
+from wishlist import views as wishlist_views
 urlpatterns = [
     path('', views.Products.as_view(), name='products'),
     path('product/add/',
@@ -17,4 +17,9 @@ urlpatterns = [
          name='product_remove_admin'),
     path('product_details/<int:pk>/update/',
          views.ProductUpdateViewAdmin.as_view(), name='product_update'),
+    path('product_details/<int:product_id>/wishlist/add',
+         wishlist_views.AddProductToWishList.as_view(), name='add_wishlist'),
+    path('product_details/<int:product_id>/wishlist/<int:wishlist_id>/remove',
+         wishlist_views.RemoveProductFromWishList.as_view(),
+         name='remove_wishlist'),
 ]
