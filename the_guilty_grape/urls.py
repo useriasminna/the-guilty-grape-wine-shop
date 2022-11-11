94fd066d832fd4e406fd633b16d474f68f967463
unknown <pal.iasmina@yahoo.com>
2022-11-11 12:14:51 +0000

    Include wishlist app urls

--- the_guilty_grape/urls.py
+++ the_guilty_grape/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from pathlib import Path
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -25,5 +24,6 @@ urlpatterns = [
     path('', include('home.urls')),
     path('subscribe/', include('newsletter.urls')),
     path('products/', include('products.urls')),
-    path('/', include('product_reviews.urls'))
+    path('/', include('product_reviews.urls')),
+    path('wishlist/', include('wishlist.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
