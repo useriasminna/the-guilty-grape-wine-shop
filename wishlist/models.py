fc1158834bb3c1cb7417517a1864887035066585
unknown <pal.iasmina@yahoo.com>
2022-11-11 11:52:55 +0000

    Add model for WishlistLine

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,17 @@
+"""
+Wishlist App - Models
+----------------
+Models for Wishlist App.
+"""
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from products.models import Product
+
+
+class WishlistLine(models.Model):
+    """Model for WishlistLine object"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        to_field='email', blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True)
