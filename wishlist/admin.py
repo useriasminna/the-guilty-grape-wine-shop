4a3d865ef585088c59b33baf25fb6e72796d58ed
unknown <pal.iasmina@yahoo.com>
2022-11-11 12:00:28 +0000

    Add custom fields display and register wishlistLine to admin panel

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,3 +1,20 @@
+"""
+Wishlist App - Admin
+----------------
+Admin Configuration for Wishlist App.
+"""
 from django.contrib import admin
 
+from wishlist.models import WishlistLine
+
+
+class WishlistLineAdmin(admin.ModelAdmin):
+    """Class for displaying categories in admin panel"""
+    list_display = (
+        'user',
+        'product',
+    )
+
+
 # Register your models here.
+admin.site.register(WishlistLine, WishlistLineAdmin)
