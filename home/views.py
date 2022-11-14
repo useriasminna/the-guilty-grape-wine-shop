d7cfe076e906703c4ae84e31d8ec3560bec81bed
unknown <pal.iasmina@yahoo.com>
2022-11-14 14:10:57 +0000

    Fix linter issues

--- home/views.py
+++ home/views.py
@@ -5,11 +5,10 @@ Views for Home App
 """
 
 from django.views.generic import TemplateView
-from django.shortcuts import render
+
 
 class Home(TemplateView):
     """
     A view to return the index page
     """
     template_name = 'home/index.html'
-    
