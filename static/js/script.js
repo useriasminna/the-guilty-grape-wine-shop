570e692b5c1f405f3291717f60155feb0794b1ac
unknown <pal.iasmina@yahoo.com>
2022-11-14 13:54:46 +0000

    Make generate stars code available fow wishlist page also

--- static/js/script.js
+++ static/js/script.js
@@ -289,6 +289,34 @@ document.addEventListener("DOMContentLoaded", function(event) {
          } );
         }
 
+    if (window.location.pathname.includes('/products/') || window.location.pathname.includes('/wishlist/')) {
+        const generateStarsContainers = document.getElementsByClassName('ratings-generated');
+            //GENERATE STARS FOR REVIEWS RATING AFTER RATE VALUE
+
+            for(let container of generateStarsContainers){
+            const rateHidden = container.previousElementSibling;
+
+        
+            for(let i=0; i<rateHidden.value; i++){
+                let star = document.createElement("button");
+                star.textContent = '★';
+                star.classList.add('star');
+                star.style.color = "#590243";
+                container.appendChild(star);
+            
+                }
+            
+                for(let i=0; i<5-rateHidden.value; i++){
+                let star = document.createElement("button");
+                star.textContent = '★';
+                star.classList.add('star');
+                star.style.color = "#80808066";
+                container.appendChild(star);
+            
+                }
+            } 
+    } 
+
     if (window.location.pathname.includes('/products/')) {  
     
         
@@ -317,31 +345,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
             
         }
 
-        const generateStarsContainers = document.getElementsByClassName('ratings-generated');
-            //GENERATE STARS FOR REVIEWS RATING AFTER RATE VALUE
-
-            for(let container of generateStarsContainers){
-            const rateHidden = container.previousElementSibling;
-
-        
-            for(let i=0; i<rateHidden.value; i++){
-                let star = document.createElement("button");
-                star.textContent = '★';
-                star.classList.add('star');
-                star.style.color = "#590243";
-                container.appendChild(star);
-            
-                }
-            
-                for(let i=0; i<5-rateHidden.value; i++){
-                let star = document.createElement("button");
-                star.textContent = '★';
-                star.classList.add('star');
-                star.style.color = "#80808066";
-                container.appendChild(star);
-            
-                }
-            } 
 
         if (window.location.pathname.includes('/product_details/')) {  
             // --------------------------SCRIPT FOR ADDING VALIDATION TO UPDATE PRODUCT FORM AVAILABLE IN PRODUCT_DETAILS.HTML--------------------------------------------- 
