508738ba43a04b13a5265ae8eadf9b8c93d46fd3
unknown <pal.iasmina@yahoo.com>
2022-11-15 23:56:59 +0200

    Add paths for Increment/Decrement quantity views

--- bag/urls.py
+++ bag/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
     path('', views.Bag.as_view(), name='bag'),
     path('add/<int:product_id>/', views.AddToBag.as_view(), name='add_to_bag'),
     path('remove/<int:product_id>/', views.RemoveFromBag.as_view(),
-         name='remove_from_bag')
+         name='remove_from_bag'),
+    path('increment/<int:product_id>/', views.IncrementQuantity.as_view(),
+         name='increment_quantity'),
+    path('decrement/<int:product_id>/', views.DecrementQuantity.as_view(),
+         name='decrement_quantity')
 ]
