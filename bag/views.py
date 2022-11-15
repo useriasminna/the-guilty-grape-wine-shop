1f333310aac86351452a7cf08ff57b135b3a712f
unknown <pal.iasmina@yahoo.com>
2022-11-15 16:03:43 +0200

    Add views for Bag template and Add Product To Bag

--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+"""
+Bag App - Views
+----------------
+Views for Bag App.
+"""
 
-# Create your views here.
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import TemplateView, View
+from django.contrib import messages
+
+from products.models import Product
+
+
+class Bag(TemplateView):
+    """A view that will render the bag page template"""
+    template_name = 'bag/bag.html'
+
+
+class AddToBag(View):
+    """A view that add the product and the corresponding quantity to the
+    shopping bag"""
+    template_name = 'bag/bag.html'
+
+    def post(self, request, product_id):
+        """Override post method"""
+        product = get_object_or_404(Product, pk=product_id)
+        quantity = int(request.POST.get('quantity'))
+        current_url = request.POST.get('current_url')
+        bag = request.session.get('bag', {})
+
+        if str(product_id) in list(bag.keys()):
+            bag[str(product_id)] += quantity
+            messages.success(request, f'Updated {product.name} quantity to\
+                {bag[str(product_id)]}')
+        else:
+            bag[product_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
+        request.session['bag'] = bag
+        return redirect(current_url)
