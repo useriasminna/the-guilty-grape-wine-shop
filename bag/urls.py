4670141f87017ec4957f95d2d9ff74757ca801a9
unknown <pal.iasmina@yahoo.com>
2022-11-15 21:34:56 +0200

    Add path for Remove product from bag view

--- bag/urls.py
+++ bag/urls.py
@@ -8,5 +8,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Bag.as_view(), name='bag'),
-    path('add/<int:product_id>/', views.AddToBag.as_view(), name='add_to_bag')
+    path('add/<int:product_id>/', views.AddToBag.as_view(), name='add_to_bag'),
+    path('remove/<int:product_id>/', views.RemoveFromBag.as_view(),
+         name='remove_from_bag')
 ]
