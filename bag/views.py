3bea9fad25bc6cc1981e8a6b746e520add04b710
unknown <pal.iasmina@yahoo.com>
2022-11-15 23:56:25 +0200

    Add views for incrementing/decrementing bag item quantity

--- bag/views.py
+++ bag/views.py
@@ -74,3 +74,60 @@ class RemoveFromBag(UserPassesTestMixin, DeleteView):
         if self.request.user.is_authenticated:
             return not self.request.user.is_superuser
         return True
+
+
+class IncrementQuantity(UserPassesTestMixin, View):
+    """A view that updates the product quantity by incrementing
+    the value with 1"""
+
+    template_name = 'bag/bag.html'
+
+    def post(self, request, product_id):
+        """Override post method"""
+        product = get_object_or_404(Product, pk=product_id)
+        current_url = request.POST.get('current_url')
+        bag = request.session.get('bag', {})
+
+        if str(product_id) in list(bag.keys()):
+            if bag[str(product_id)] < product.stock:
+                bag[str(product_id)] += 1
+            else:
+                messages.info(request, f'Limited stock. Cannot order more than\
+                    {bag[str(product_id)]} items for {product.name}.')
+        else:
+            messages.error(request, f'{product.name} is not in your bag.\
+                           Quantity update failed')
+        request.session['bag'] = bag
+        return redirect(current_url)
+
+    def test_func(self):
+        if self.request.user.is_authenticated:
+            return not self.request.user.is_superuser
+        return True
+
+
+class DecrementQuantity(UserPassesTestMixin, View):
+    """A view that updates the product quantity by decrementing
+    the value with 1"""
+
+    template_name = 'bag/bag.html'
+
+    def post(self, request, product_id):
+        """Override post method"""
+        product = get_object_or_404(Product, pk=product_id)
+        current_url = request.POST.get('current_url')
+        bag = request.session.get('bag', {})
+
+        if str(product_id) in list(bag.keys()):
+            if bag[str(product_id)] > 1:
+                bag[str(product_id)] -= 1
+        else:
+            messages.error(request, f'{product.name} is not in your bag.\
+                        Quantity update failed')
+        request.session['bag'] = bag
+        return redirect(current_url)
+
+    def test_func(self):
+        if self.request.user.is_authenticated:
+            return not self.request.user.is_superuser
+        return True
