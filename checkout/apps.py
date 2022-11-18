d9adc178e01f250ab32edc060d2f3c7f5f0f6e20
unknown <pal.iasmina@yahoo.com>
2022-11-18 16:42:24 +0200

    Import checkout signals

--- checkout/apps.py
+++ checkout/apps.py
@@ -1,6 +1,14 @@
+
+"""
+Checkout App - Apps
+----------------
+Apps configuration for Checkout App.
+"""
 from django.apps import AppConfig
 
 
 class CheckoutConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
+
+    def ready(self):
+        import checkout.signals
