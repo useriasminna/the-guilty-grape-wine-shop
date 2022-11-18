ddf91685ad5508b0c17a74fb0ffe3db59cceca3f
unknown <pal.iasmina@yahoo.com>
2022-11-18 16:40:59 +0200

    Add view for checkout template rendering

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+"""
+Checkout App - Views
+----------------
+Views for Checkout App.
+"""
 
+from django.views.generic import TemplateView
+from .models import Order
+from .forms import OrderForm
 # Create your views here.
+
+
+class Checkout(TemplateView):
+    template_name = 'checkout/checkout.html'
+    model = Order
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['order_form'] = OrderForm
+        return context
