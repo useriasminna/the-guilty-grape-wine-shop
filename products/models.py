ab206e310bd73dc13666a3ce95f27c7dc80c7aa8
unknown <pal.iasmina@yahoo.com>
2022-11-19 16:46:02 +0200

    Update country field and add required properties to fields

--- products/models.py
+++ products/models.py
@@ -4,6 +4,7 @@ Products App - Models
 Models for Products App.
 """
 from django.db import models
+from django_countries.fields import CountryField
 
 
 class Category(models.Model):
@@ -16,7 +17,7 @@ class Category(models.Model):
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
-        return str(self.name)
+        return str(self.friendly_name)
 
     def get_friendly_name(self):
         """Method that returns category friendly_name"""
@@ -29,19 +30,20 @@ class Product(models.Model):
                                  on_delete=models.SET_NULL)
     is_deluxe = models.BooleanField()
     sku = models.CharField(max_length=254, unique=True)
-    name = models.CharField(max_length=254)
-    region = models.TextField(max_length=100)
-    country = models.TextField(max_length=100)
-    grapes = models.TextField(max_length=254)
-    year = models.IntegerField()
-    style = models.TextField()
+    name = models.CharField(max_length=254, null=False, blank=False)
+    region = models.CharField(max_length=100, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
+    grapes = models.CharField(max_length=254, null=False, blank=False)
+    year = models.IntegerField(null=False, blank=False)
+    style = models.CharField(max_length=50, null=False, blank=False)
     code = models.CharField(max_length=6, unique=True)
-    food_pairing = models.TextField(max_length=60)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    food_pairing = models.CharField(max_length=60, null=False, blank=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=False,
+                                blank=False)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
                                  blank=True)
     image = models.ImageField(null=True, blank=True)
-    stock = models.IntegerField(default=1000000)
+    stock = models.IntegerField(null=False, blank=False)
 
     def __str__(self):
         return str(self.name)
