33ce3e2ec405786b4366501a3836c0a71c99935c
unknown <pal.iasmina@yahoo.com>
2022-11-21 16:57:39 +0200

    Update order field for OrderLine model

--- checkout/models.py
+++ checkout/models.py
@@ -75,7 +75,8 @@ class Order(models.Model):
 
 class OrderLine(models.Model):
     order = models.ForeignKey(
-        Order, null=False, blank=False, on_delete=models.CASCADE)
+        Order, null=False, blank=False, on_delete=models.CASCADE,
+        related_name='lineitems')
     product = models.ForeignKey(
         Product, null=False, blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
