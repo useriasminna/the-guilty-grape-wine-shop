457199090b04959797a731eb653560299f6caa31
unknown <pal.iasmina@yahoo.com>
2022-11-22 10:17:57 +0200

    Remove print statement

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -14,7 +14,6 @@ def webhook(request):
     """Listen for webhooks from Stripe"""
     # Setup
     wh_secret = settings.STRIPE_WH_SECRET
-    print(wh_secret)
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
     # Get the webhook data and verify its signature
