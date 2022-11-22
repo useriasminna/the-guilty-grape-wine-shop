a1501a123ffcdc16aed53a6435057d7e61fd567b
unknown <pal.iasmina@yahoo.com>
2022-11-22 10:17:18 +0200

    Add paths for webhook method and Cache Checkout Data view

--- checkout/urls.py
+++ checkout/urls.py
@@ -5,9 +5,13 @@ Urls for Checkout App.
 """
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.Checkout.as_view(), name='checkout'),
     path('checkout_success/<order_number>', views.CheckoutSuccess.as_view(),
          name='checkout_success'),
+    path('cache_checkout_data/', views.CacheCheckoutData.as_view(),
+         name='cache_checkout_data'),
+    path('wh/', webhook, name='webhook'),
 ]
