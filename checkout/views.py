129aa407f452f5a21bd67ba7b1acac887ba196ef
unknown <pal.iasmina@yahoo.com>
2022-11-23 21:35:16 +0200

    Update Chechout succes view to update the user profile with delivery details

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ from django.conf import settings
 from .models import Order, OrderLine
 from .forms import OrderForm
 from products.models import Product
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 from bag.contexts import bag_contents
 
 import stripe
@@ -138,6 +140,30 @@ class CheckoutSuccess(View):
     def get(self, request, order_number):
         """Override get method"""
         order = get_object_or_404(Order, order_number=order_number)
+        save_info = request.session.get('save_info')
+
+        if request.user.is_authenticated:
+            profile = UserProfile.objects.get(user=request.user)
+            # Attach the user's profile to the order
+            order.user = profile
+            order.save()
+
+            # Save the user's info
+            if save_info:
+                profile_data = {
+                    'default_phone_number': order.phone_number,
+                    'default_country': order.country,
+                    'default_postcode': order.postcode,
+                    'default_town_or_city': order.town_or_city,
+                    'default_street_address1': order.street_address1,
+                    'default_street_address2': order.street_address2,
+                    'default_county': order.county,
+                }
+                user_profile_form = UserProfileForm(profile_data,
+                                                    instance=profile)
+                if user_profile_form.is_valid():
+                    user_profile_form.save()
+
         messages.success(request, f'Order successfully processed! \
             Your order number is {order_number}. A confirmation \
             email will be sent to {order.email}.')
