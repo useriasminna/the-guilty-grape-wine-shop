18f11ca73875278e67836582661956b85da96db3
unknown <pal.iasmina@yahoo.com>
2022-11-24 15:51:55 +0200

    Add code to update profile information if save_info was checked

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -8,6 +8,7 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLine
 from products.models import Product
+from profiles.models import UserProfile
 
 import json
 import time
@@ -35,6 +36,7 @@ class StripeWH_Handler:
         intent = event.data.object
         pid = intent.id
         bag = intent.metadata.bag
+        save_info = intent.metadata.save_info
         stripe_charge = stripe.Charge.retrieve(
             intent.latest_charge
         )
@@ -47,6 +49,24 @@ class StripeWH_Handler:
         for field, value in shipping_details.address.items():
             if value == "":
                 shipping_details.address[field] = None
+
+        # Update profile information if save_info was checked
+        profile = None
+        profile = None
+        email = intent.metadata.email
+        if email != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__email=email)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 =\
+                    shipping_details.address.line1
+                profile.default_street_address2 =\
+                    shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
+                profile.save()
 
         order_exists = False
         attempt = 1
@@ -81,6 +101,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
