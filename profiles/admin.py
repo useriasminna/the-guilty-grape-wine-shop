a060fe36d762978fbdb8df290a77fd9aeb10a59f
unknown <pal.iasmina@yahoo.com>
2022-11-24 12:04:18 +0200

    Registe UserProfile model to admin panel

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,9 @@
+"""
+Profiles App - Admin
+----------------
+Admin configuration for Profiles App.
+"""
 from django.contrib import admin
-
+from .models import UserProfile
 # Register your models here.
+admin.site.register(UserProfile)
