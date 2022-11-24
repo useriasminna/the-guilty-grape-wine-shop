88f8ae58591b9341ea84335cc02578094d025475
unknown <pal.iasmina@yahoo.com>
2022-11-24 11:39:09 +0200

    Remove stripe class from fields

--- checkout/forms.py
+++ checkout/forms.py
@@ -40,5 +40,4 @@ class OrderForm(forms.ModelForm):
                 else:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
