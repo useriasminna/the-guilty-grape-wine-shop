5cba12d4080aa7a7169a197a08cdc6341bf92001
unknown <pal.iasmina@yahoo.com>
2022-11-25 02:22:38 +0200

    Update bag context to include discount calculation

--- bag/contexts.py
+++ bag/contexts.py
@@ -10,6 +10,7 @@ from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from products.models import Product
 from wishlist.models import WishlistLine
+from vouchers.models import Voucher
 
 
 def bag_contents(request):
@@ -17,6 +18,9 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    discount_value = 0
+    voucher_id = request.session.get('voucher_id', None)
+    discount = request.session.get('discount', None)
     bag = request.session.get('bag', {})
 
     # GET LIST OF BAG ITEMS WITH PROPERTIES: PRODUCT, QUANTITY AND SUBTOTAL
@@ -41,20 +45,38 @@ def bag_contents(request):
             'current_wishlist_line': current_wishlist_line
         })
 
+    if discount:
+        # CALCULATE DISCOUNT VALUE
+        discount_value = total * Decimal(discount / 100)
+        if (total - discount_value) < settings.FREE_DELIVERY_THRESHOLD:
+            delivery = (total - discount_value) * Decimal(
+                settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+            free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - (
+                total - discount_value)
+        else:
+            delivery = 0
+            free_delivery_delta = 0
+        grand_total = (total - discount_value) + delivery
     # CALCULATE DELIVERY AND FREE DELIVERY DELTA VALUE
-    if total < settings.FREE_DELIVERY_THRESHOLD:
+    elif total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+        grand_total = delivery + total
     else:
         delivery = 0
         free_delivery_delta = 0
+        grand_total = delivery + total
 
-    grand_total = delivery + total
-
+    if voucher_id:
+        voucher = Voucher.objects.get(pk=voucher_id)
+    else:
+        voucher = None
     context = {
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
+        'voucher': voucher,
+        'discount_value': discount_value,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
