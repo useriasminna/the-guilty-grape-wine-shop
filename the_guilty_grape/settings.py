51fceb4fbcade70c1b8cc013778a4790360b6668
unknown <pal.iasmina@yahoo.com>
2022-11-29 11:19:07 +0200

    Set debug to False

--- the_guilty_grape/settings.py
+++ the_guilty_grape/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['the-guilty-grape-wine-shop.herokuapp.com', 'localhost',
                  '127.0.0.1']
